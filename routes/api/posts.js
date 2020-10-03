33d8f754c66ff5f4718f51f0270b1afc73b1884b
cicerofonseca <cicerorfonseca@gmail.com>
2020-10-03 08:58:49 -0400

    Create like/unlike post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,4 +41,133 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    // date-1 to return most recent posts
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error getting all posts...');
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found...' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    // To check if the post id is invalid
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found...' });
+    }
+
+    res.status(500).send('Server error getting all posts...');
+  }
+});
+
+// @route   DELETE api/posts
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // To check if the post id is invalid
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found...' });
+    }
+
+    // Check user post x user logged
+    if (post.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'User not authorized to delete this post...' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed!' });
+  } catch (err) {
+    console.error(err.message);
+
+    // To check if the post id is invalid
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found...' });
+    }
+
+    res.status(500).send('Server error getting all posts...');
+  }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has already been liked by this user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error liking the post...');
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has already been liked by this user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get the remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error liking the post...');
+  }
+});
+
 module.exports = router;
