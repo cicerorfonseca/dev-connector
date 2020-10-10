11c0b8a03d49e398a82d70a3d0510341a4dddb55
cicerofonseca <cicerorfonseca@gmail.com>
2020-10-10 06:37:24 -0400

    Create dashboard protected route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Auth from './components/auth/Register';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -41,6 +43,8 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              {/* Protected route. This way the app will redirect the user if not authenticated */}
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
